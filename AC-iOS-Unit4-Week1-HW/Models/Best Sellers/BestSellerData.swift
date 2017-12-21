332456966202d9d2e7bb94b376d0478a02689b01
Melissa He <melissahe@ac.c4q.nyc>
2017-12-20 21:39:13 -0500

    Add best seller persistent data model

--- AC-iOS-Unit4-Week1-HW/Models/Best Sellers/BestSellerData.swift
+++ AC-iOS-Unit4-Week1-HW/Models/Best Sellers/BestSellerData.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+class BestSellerData {
+    private init() {}
+    static let manager = BestSellerData()
+    
+    //save
+    func saveBestSellers(_ bestSellers: [BestSeller], inCategory category: String) {
+        
+        PersistentData.manager.saveItem(bestSellers, atFileName: pListName(ofCategory: category))
+    }
+    
+    //get
+    func getBestSellers(inCategory category: String) -> [BestSeller]? {
+        
+        guard let bestSellers = PersistentData.manager.loadItems(fromFileName: pListName(ofCategory: category)) as? [BestSeller] else {
+            return nil
+        }
+        
+        return bestSellers
+    }
+    
+    private func pListName(ofCategory category: String) -> String {
+        return category + ".plist"
+    }
+    
+}
